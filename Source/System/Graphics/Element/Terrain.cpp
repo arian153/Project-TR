8966c2adb9ffbb8399cf78e9bd13c926aed9b356
Eric Han <jangseok.han@digipen.edu>
2021-09-28 23:47:47 -0700

    Added terrain generation code little bit.

--- Source/System/Graphics/Element/Terrain.cpp
+++ Source/System/Graphics/Element/Terrain.cpp
@@ -2,6 +2,8 @@
 
 #include "../Common/Buffer2/IndexBufferCommon.hpp"
 #include "../Common/Buffer2/VertexBufferCommon.hpp"
+#include "../DataType/MeshData.hpp"
+#include "../Utility/MeshGenerator.hpp"
 
 namespace GAM400
 {
@@ -45,5 +47,53 @@ namespace GAM400
             delete m_vertex_buffer;
             m_vertex_buffer = nullptr;
         }
+    }
+
+    void Terrain::Build()
+    {
+        size_t size = m_grid.vertices.size();
+
+        for (size_t i = 0; i < size; ++i)
+        {
+            Vector3 p = m_grid.vertices[i].GetPosition();
+            p.y       = GenerateTrigonometricHeight(p.x, p.z);
+
+            m_grid.vertices[i].SetPosition(p);
+            m_grid.vertices[i].SetNormal(GenerateTrigonometricNormal(p.x, p.y));
+            m_grid.vertices[i].CalculateTangentAndBinormal();
+        }
+
+        if (m_terrain_vertex_size == size)
+        {
+            //use dynamic buffer
+        }
+        else
+        {
+            //resize vertex & index buffer
+        }
+    }
+
+    Real Terrain::GenerateTrigonometricHeight(Real x, Real z) const
+    {
+        return m_trigonometric_factor_a * (z * sinf(m_trigonometric_factor_b * x) + x * cosf(m_trigonometric_factor_b * z));
+    }
+
+    Vector3 Terrain::GenerateTrigonometricNormal(Real x, Real z) const
+    {
+        Real ab = m_trigonometric_factor_a * m_trigonometric_factor_b;
+
+        Vector3 normal(
+                       -ab * z * cosf(m_trigonometric_factor_b * x) - m_trigonometric_factor_a * cosf(m_trigonometric_factor_b * z),
+                       1.0f,
+                       -m_trigonometric_factor_a * sinf(m_trigonometric_factor_b * x) + ab * x * sinf(m_trigonometric_factor_b * z));
+
+        return normal.Unit();
+    }
+
+    void Terrain::ClearGrid()
+    {
+        MeshGenerator mesh_generator;
+        mesh_generator.CreateGrid(m_terrain_width, m_terrain_depth, 50, 50, m_grid);
+        m_terrain_vertex_size = m_grid.vertices.size();
     }
 }
--- Source/System/Graphics/Element/Terrain.hpp
+++ Source/System/Graphics/Element/Terrain.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "../DataType/MeshData.hpp"
 
 namespace GAM400
 {
@@ -16,8 +17,23 @@ namespace GAM400
         void CreateBuffer();
         void ReleaseBuffer();
 
-    private:
+        void Build();
 
+        Real GenerateTrigonometricHeight(Real x, Real z) const;
+        Vector3 GenerateTrigonometricNormal(Real x, Real z) const;
+
+        void ClearGrid();
+
+    private:
+        Real m_terrain_width = 200.0f;
+        Real m_terrain_depth = 200.0f;
+        size_t m_terrain_vertex_size = 0;
+        
+
+        Real m_trigonometric_factor_a = 0.3f;
+        Real m_trigonometric_factor_b = 0.1f;
+
+        MeshData            m_grid;
         VertexBufferCommon* m_vertex_buffer = nullptr;
         IndexBufferCommon*  m_index_buffer  = nullptr;
     };
